6e5266c52735201e0f2d61b4c0c63831e8a55fc9
Hai Duong <caubenondo@gmail.com>
2022-08-17 20:52:20 -0700

    add and install package json

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,81 +1,83 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 // SOVLED
 module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      // HTML loader to create index
-      new HtmlWebpackPlugin({
-        template: "./index.html",
-        title: "JATE - Just Another Text Editor",
-      }),
-      // adding Service Workers from src-sw.js
-      new InjectManifest({
-        swSrc: "./src-sw.js",
-        swDest: "src-sw.js",
-      }),
-      // adding manifest
-      new WebpackPwaManifest({
-        fingerprints: false,
-        inject: true,
-        name: "Just Another Text Editor",
-        short_name: "JATE",
-        description: "Text Editor with offline capabilities using IndexedDB",
-        background_color: "#225ca3",
-        theme_color: "#225ca3",
-        start_url: "/",
-        publicPath: "/",
-        icons: [
-          {
-            src: path.resolve("src/images/logo.png"),
-            sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join("assets", "icons"),
-          },
-          {src: path.resolve('favicon.ico'),
-        destination: path.join("assets"),
-      size:48}
-        ],
-      }),
-    ],
-
-    module: {
-      
-      rules: [
-        // add style loader
-        {
-          test: /\.css$/,
-          use: ["style-loader", "css-loader"],
+    return {
+        mode: "development",
+        entry: {
+            main: "./src/js/index.js",
+            install: "./src/js/install.js",
         },
-        // add babel
-        {
-          test: /\.m?js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: ["@babel/preset-env"],
-              plugins: [
-                "@babel/plugin-proposal-object-rest-spread",
-                "@babel/transform-runtime",
-              ],
-            },
-          }
-        }
-      ],
-    },
-  };
+        output: {
+            filename: "[name].bundle.js",
+            path: path.resolve(__dirname, "dist"),
+        },
+        plugins: [
+            // HTML loader to create index
+            new HtmlWebpackPlugin({
+                template: "./index.html",
+                title: "JATE - Just Another Text Editor",
+            }),
+            // adding Service Workers from src-sw.js
+            new InjectManifest({
+                swSrc: "./src-sw.js",
+                swDest: "src-sw.js",
+            }),
+            // adding manifest
+            new WebpackPwaManifest({
+                fingerprints: false,
+                inject: true,
+                name: "Just Another Text Editor",
+                short_name: "JATE",
+                description:
+                    "Text Editor with offline capabilities using IndexedDB and Service Worker",
+                background_color: "#225ca3",
+                theme_color: "#225ca3",
+                start_url: "/",
+                publicPath: "/",
+                icons: [
+                    {
+                        src: path.resolve("src/images/logo.png"),
+                        sizes: [96, 128, 192, 256, 384, 512],
+                        destination: path.join("assets", "icons"),
+                    },
+                    {
+                        src: path.resolve("favicon.ico"),
+                        destination: path.join("assets"),
+                        size: 48,
+                    },
+                ],
+            }),
+        ],
+
+        module: {
+            rules: [
+                // add style loader
+                {
+                    test: /\.css$/,
+                    use: ["style-loader", "css-loader"],
+                },
+                // add babel
+                {
+                    test: /\.m?js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ["@babel/preset-env"],
+                            plugins: [
+                                "@babel/plugin-proposal-object-rest-spread",
+                                "@babel/transform-runtime",
+                            ],
+                        },
+                    },
+                },
+            ],
+        },
+    };
 };
