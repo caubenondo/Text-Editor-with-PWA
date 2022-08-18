272ab8d7304e8d81b49999e3278123758ecf9ade
Hai Duong <caubenondo@gmail.com>
2022-08-17 20:27:31 -0700

    solve the installation button

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,62 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
-    upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
-        return;
-      }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
-    },
-  });
+    openDB("jate", 1, {
+        upgrade(db) {
+            if (db.objectStoreNames.contains("jate")) {
+                console.log("jate database already exists");
+                return;
+            }
+            db.createObjectStore("jate", {
+                keyPath: "id",
+                autoIncrement: true,
+            });
+            console.log("jate database created");
+        },
+    });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// SETTER
+export const putDb = async (content) => {
+    // save stuff to indexDB
+    try {
+        // connect idb
+        const jateDB = await openDB("jate", 1);
+        // make new transaction with read and write permit
+        const tx = jateDB.transaction("jate", "readwrite");
+        // open object store
+        const store = tx.objectStore("jate");
+        // put content into store 
+        // without specifying Key, it will write new key/value every time it saved
+        const request = store.put({ id:1, value: content });
+        const result = await request;
+
+        console.log("Data successfully saved to database", result);
+    } catch (error) {
+        console.error("putDb not implemented", error);
+    }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// GETTER
+export const getDb = async () => {
+  try {
+    // connect idb
+    const jateDB = await openDB("jate", 1);
+    // make new transaction with read and write permit
+    const tx = jateDB.transaction("jate", "readwrite");
+    // open object store
+    const store = tx.objectStore("jate");
+    // get values stored from idb
+    // since the main getDB did not specify database key, we getAll
+    const request = store.getAll();
+    const result = await request;
+    console.log('data got from iDB: ', result)
+  } catch (error) {
+    console.error("getDb not implemented", error);
+  }
+
+}
+
 
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,40 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener("beforeinstallprompt", (event) => {
+    event.preventDefault();
+    console.log("👍", "beforeinstallprompt", event);
+    window.deferredPrompt = event;
+    // show the button
+    butInstall.classList.toggle("hidden", false);
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener("click", async () => {
+    
+    console.log("👍", "butInstall-clicked");
+
+    const prompt = window.deferredPrompt;
+    if (!prompt) {
+        // deferred prompt isnt available
+        return;
+    }
+    // show install prompt
+    prompt.prompt();
+    // Log the result
+    const result = await prompt.userChoice;
+    console.log("👍", "userChoice", result);
+    window.deferredPrompt = null;
+    // hide the button 
+    butInstall.classList.toggle("hidden",true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener("appinstalled", (event) => {
+    console.log("👍", "appinstalled", event);
+    // hide the button
+    butInstall.classList.toggle("hidden", true);
+    // Clear the deferredPrompt so it can be garbage collected
+    window.deferredPrompt = null;
+});
